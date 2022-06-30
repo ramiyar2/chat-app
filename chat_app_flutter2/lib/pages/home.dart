c077e3d77f6724a4825f1f18df92e5ac538c5ca9
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-01 00:24:35 +0300

    cleaning code

--- chat_app_flutter2/lib/pages/home.dart
+++ chat_app_flutter2/lib/pages/home.dart
@@ -45,167 +45,152 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
-              items.isNotEmpty
-                  ? Expanded(
-                      child: Container(
-                        height: 537,
-                        decoration: BoxDecoration(
-                            color: darker_blue,
-                            borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(40.0),
-                                topLeft: Radius.circular(40.0))),
-                        child: SingleChildScrollView(
-                          child: Wrap(
-                            children: [
-                              Container(
-                                margin:
-                                    const EdgeInsets.only(top: 20, left: 25),
-                                child: Text(
-                                  'Stores',
-                                  style: TextStyle(color: green, fontSize: 20),
-                                ),
-                              ),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      margin: const EdgeInsets.only(
-                                          top: 20, left: 20),
-                                      width: size.width,
-                                      height: 60,
-                                      child: ListView.separated(
-                                          scrollDirection: Axis.horizontal,
-                                          shrinkWrap: true,
-                                          itemBuilder: (BuildContext context,
-                                                  int index) =>
-                                              Container(
-                                                width: 60,
-                                                height: 60,
-                                                decoration: BoxDecoration(
-                                                    image: DecorationImage(
-                                                        image: NetworkImage(
-                                                            store[index]
-                                                                ['img']),
-                                                        fit: BoxFit.cover),
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            60)),
-                                              ),
-                                          separatorBuilder:
-                                              (BuildContext context,
-                                                      int index) =>
-                                                  const SizedBox(
-                                                    width: 20,
-                                                  ),
-                                          itemCount: store.length),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                margin:
-                                    const EdgeInsets.only(top: 20, left: 25),
-                                child: Text(
-                                  'Chats',
-                                  style: TextStyle(color: green, fontSize: 20),
-                                ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(
-                                  top: 20,
-                                ),
-                                height: 600,
-                                child: ListView.builder(
-                                  itemCount: items.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return Card(
-                                      color: dark_blue,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        side: BorderSide(
-                                          color: green,
-                                          width: 2.0,
-                                        ),
-                                      ),
-                                      child: ListTile(
-                                        title: Text(items[index]["userName"]),
-                                        subtitle: Text(
-                                          items[index]["massage"],
-                                          style: TextStyle(
-                                            color:
-                                                items[index]["unread"] == null
-                                                    ? Color.fromRGBO(
-                                                        255, 255, 255, 0.5)
-                                                    : green,
-                                          ),
-                                        ),
-                                        trailing: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.end,
-                                          children: [
-                                            Text(
-                                              items[index]["time"].toString(),
-                                              style: TextStyle(
-                                                color: items[index]["unread"] ==
-                                                        null
-                                                    ? Color.fromRGBO(
-                                                        255, 255, 255, 0.5)
-                                                    : green,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              height: 10,
-                                            ),
-                                            Container(
-                                              width: 25,
-                                              height: 25,
-                                              child: items[index]["unread"] ==
-                                                      null
-                                                  ? Container()
-                                                  : CircleAvatar(
-                                                      backgroundColor: green,
-                                                      child: Text(
-                                                        items[index]["unread"]
-                                                            .toString(),
-                                                        style: TextStyle(
-                                                            color: darker_blue),
-                                                      ),
-                                                    ),
-                                            ),
-                                          ],
-                                        ),
-                                        leading: Container(
-                                          width: 60,
-                                          height: 60,
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(60),
-                                              image: DecorationImage(
-                                                fit: BoxFit.cover,
-                                                image: NetworkImage(
-                                                  items[index]["img"],
-                                                ),
-                                              )),
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  : Container(),
+              items.isNotEmpty ? ChatAndStores(size) : Container(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Expanded ChatAndStores(Size size) {
+    return Expanded(
+      child: Container(
+        height: 537,
+        decoration: BoxDecoration(
+            color: darker_blue,
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(40.0),
+                topLeft: Radius.circular(40.0))),
+        child: SingleChildScrollView(
+          child: Wrap(
+            children: [
+              Title('Stores'),
+              Stores(size),
+              Title('Chats'),
+              Chats(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView Stores(Size size) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 20),
+            width: size.width,
+            height: 60,
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) => Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(store[index]['img']),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(60)),
+                    ),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const SizedBox(
+                      width: 20,
+                    ),
+                itemCount: store.length),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container Title(String title) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 25),
+      child: Text(
+        title,
+        style: TextStyle(color: green, fontSize: 20),
+      ),
+    );
+  }
+
+  Container Chats() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 20,
+      ),
+      height: 600,
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: dark_blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: BorderSide(
+                color: green,
+                width: 2.0,
+              ),
+            ),
+            child: ListTile(
+              title: Text(items[index]["userName"]),
+              subtitle: Text(
+                items[index]["massage"],
+                style: TextStyle(
+                  color: items[index]["unread"] == null
+                      ? Color.fromRGBO(255, 255, 255, 0.5)
+                      : green,
+                ),
+              ),
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    items[index]["time"].toString(),
+                    style: TextStyle(
+                      color: items[index]["unread"] == null
+                          ? Color.fromRGBO(255, 255, 255, 0.5)
+                          : green,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 25,
+                    height: 25,
+                    child: items[index]["unread"] == null
+                        ? Container()
+                        : CircleAvatar(
+                            backgroundColor: green,
+                            child: Text(
+                              items[index]["unread"].toString(),
+                              style: TextStyle(color: darker_blue),
+                            ),
+                          ),
+                  ),
+                ],
+              ),
+              leading: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        items[index]["img"],
+                      ),
+                    )),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
