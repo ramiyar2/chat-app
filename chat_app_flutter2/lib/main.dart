d8753ce2321328286ab3a2e24698b9d11d8b8520
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-03 07:01:09 +0300

    Update main.dart

--- chat_app_flutter2/lib/main.dart
+++ chat_app_flutter2/lib/main.dart
@@ -6,9 +6,6 @@ import 'pages/home.dart';
 import 'pages/verify.dart';
 
 void main() => runApp(const MyApp());
-/*
-# hi from github 
-*/
 var green = const Color.fromRGBO(30, 253, 119, 1);
 var dark_green = const Color.fromRGBO(0, 154, 62, 1);
 var dark_blue = const Color.fromRGBO(24, 42, 43, 1);
