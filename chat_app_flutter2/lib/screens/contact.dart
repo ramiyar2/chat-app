966e22131b69485151a059843957273a904465e0
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-16 14:05:53 +0300

    Contacts page ui

--- chat_app_flutter2/lib/screens/contact.dart
+++ chat_app_flutter2/lib/screens/contact.dart
@@ -12,60 +12,61 @@ class Contact extends StatelessWidget {
     String _call =
         "https://img.icons8.com/fluency-systems-regular/48/FFFFFF/phone-disconnected.png";
     return Wrap(
-      children: [
-        Title('Contact'),
-        Container(
-          margin: EdgeInsets.only(top: 20),
-          height: 600,
-          child: ListView.separated(
-              itemBuilder: (BuildContext context, int index) => ListTile(
-                    title: Text(contact[index]['userName']),
-                    subtitle: Text(
-                      contact[index]['store'],
-                      style: TextStyle(color: green),
-                    ),
-                    leading: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(60),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                              contact[index]["img"],
-                            ),
-                          )),
-                    ),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Image.network(
-                              _video,
-                              width: 28,
-                              color: Colors.white,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Image.network(
-                              _call,
-                              width: 28,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+      children: [Title('Contact'), ContactsList(_video, _call)],
+    );
+  }
+
+  Container ContactsList(String _video, String _call) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      height: 600,
+      child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) => ListTile(
+                title: Text(contact[index]['userName']),
+                subtitle: Text(
+                  contact[index]['store'],
+                  style: TextStyle(color: green),
+                ),
+                leading: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          contact[index]["img"],
+                        ),
+                      )),
+                ),
+                trailing: SizedBox(
+                  width: 100,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.network(
+                          _video,
+                          width: 28,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.network(
+                          _call,
+                          width: 28,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-              separatorBuilder: (BuildContext context, int index) => SizedBox(
-                    height: 20,
-                  ),
-              itemCount: contact.length),
-        )
-      ],
+                ),
+              ),
+          separatorBuilder: (BuildContext context, int index) => SizedBox(
+                height: 20,
+              ),
+          itemCount: contact.length),
     );
   }
 }
