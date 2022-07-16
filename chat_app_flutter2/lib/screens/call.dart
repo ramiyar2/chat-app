fcc63631af81d2aacc92981ef79f701f3245c77b
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-16 13:45:17 +0300

    call page ui

--- chat_app_flutter2/lib/screens/call.dart
+++ chat_app_flutter2/lib/screens/call.dart
@@ -16,61 +16,65 @@ class Call extends StatelessWidget {
         "https://img.icons8.com/external-dreamstale-lineal-dreamstale/32/000000/external-up-arrows-dreamstale-lineal-dreamstale-5.png";
     String _down =
         "https://img.icons8.com/material-rounded/24/000000/down3.png";
+
     return SingleChildScrollView(
       child: Wrap(
         children: [
           Title('Stores'),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            height: 600,
-            child: ListView.separated(
-                itemBuilder: (BuildContext context, int index) => ListTile(
-                      title: Text(call[index]['userName']),
-                      subtitle: Row(
-                        children: [
-                          Image.network(
-                            call[index]['answered'] == true ? _up : _down,
-                            width: 12,
-                            color: call[index]['answered'] == true
-                                ? green
-                                : Colors.red,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            call[index]['time'],
-                            style: TextStyle(color: green),
-                          )
-                        ],
-                      ),
-                      leading: Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(60),
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                call[index]["img"],
-                              ),
-                            )),
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {},
-                        icon: Image.network(
-                          _call,
-                          width: 28,
-                        ),
-                      ),
-                    ),
-                separatorBuilder: (BuildContext context, int index) => SizedBox(
-                      height: 20,
-                    ),
-                itemCount: call.length),
-          ),
+          CallsList(_up, _down, _call),
         ],
       ),
+    );
+  }
+
+  Container CallsList(String _up, String _down, String _call) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      height: 600,
+      child: ListView.separated(
+          itemBuilder: (BuildContext context, int index) => ListTile(
+                title: Text(call[index]['userName']),
+                subtitle: Row(
+                  children: [
+                    Image.network(
+                      call[index]['answered'] == true ? _up : _down,
+                      width: 12,
+                      color:
+                          call[index]['answered'] == true ? green : Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      call[index]['time'],
+                      style: TextStyle(color: green),
+                    )
+                  ],
+                ),
+                leading: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          call[index]["img"],
+                        ),
+                      )),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Image.network(
+                    _call,
+                    width: 28,
+                  ),
+                ),
+              ),
+          separatorBuilder: (BuildContext context, int index) => SizedBox(
+                height: 20,
+              ),
+          itemCount: call.length),
     );
   }
 }
