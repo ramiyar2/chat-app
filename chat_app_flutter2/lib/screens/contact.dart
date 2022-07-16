a4bf02c0f9315519f9ec622faa50449e59b8b1e3
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-16 13:56:23 +0300

    data

--- chat_app_flutter2/lib/screens/contact.dart
+++ chat_app_flutter2/lib/screens/contact.dart
@@ -5,6 +5,9 @@ class Contact extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String _video = "https://img.icons8.com/ios/50/000000/video-call.png";
+    String _call =
+        "https://img.icons8.com/fluency-systems-regular/48/FFFFFF/phone-disconnected.png";
     return Center(
       child: Text(
         'hello',
