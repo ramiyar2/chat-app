050ea279d1b019ca9ab9f1105701effd6b4c03bf
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-16 13:56:49 +0300

    data

--- chat_app_flutter2/lib/data/data.dart
+++ chat_app_flutter2/lib/data/data.dart
@@ -96,50 +96,36 @@ List<Map> store = [
     "id": 1,
     "userName": "Olivia",
     "img": "http://placeimg.com/640/480/any",
-    "time": "Now",
-    "unread": 1,
-    "massage": "ok , c u tonight?"
   },
   {
     "userId": 2,
     "id": 2,
     "userName": "Aadeel",
     "img": "http://placeimg.com/640/480/any",
-    "time": "10:10 PM",
-    "unread": 1,
-    "massage": "how are you"
   },
   {
     "userId": 3,
     "id": 3,
     "userName": "Emma",
     "img": "http://placeimg.com/640/480/any",
-    "time": "05:14 PM",
-    "massage": "yes today is meeting"
   },
   {
     "userId": 4,
     "id": 4,
     "userName": "Alai",
     "img": "http://placeimg.com/640/480/any",
-    "time": "04:45 PM",
-    "massage": "byee 😊"
   },
   {
     "userId": 5,
     "id": 5,
     "userName": "Abba",
     "img": "http://placeimg.com/640/480/any",
-    "time": "04:40 PM",
-    "massage": "ok"
   },
   {
     "userId": 6,
     "id": 6,
     "userName": "Sophia",
     "img": "http://placeimg.com/640/480/any",
-    "time": "yesterday",
-    "massage": "i miss you"
   },
   {
     "userId": 7,
@@ -154,32 +140,24 @@ List<Map> store = [
     "id": 8,
     "userName": "Adnan",
     "img": "http://placeimg.com/640/480/any",
-    "time": "yesterday",
-    "massage": "Hi"
   },
   {
     "userId": 9,
     "id": 9,
     "userName": "Adnan",
     "img": "http://placeimg.com/640/480/any",
-    "time": "yesterday",
-    "massage": "Hi"
   },
   {
     "userId": 10,
     "id": 10,
     "userName": "Adnan",
     "img": "http://placeimg.com/640/480/any",
-    "time": "yesterday",
-    "massage": "Hi"
   },
   {
     "userId": 11,
     "id": 11,
     "userName": "Adnan",
     "img": "http://placeimg.com/640/480/any",
-    "time": "yesterday",
-    "massage": "Hi"
   }
 ];
 List<Map> call = [
@@ -206,18 +184,77 @@ List<Map> contact = [
     "id": 1,
     "userName": "Olivia",
     "img": "http://placeimg.com/640/480/any",
-    "time": "Now",
-    "unread": 1,
-    "massage": "ok , c u tonight?"
+    "store": "At gym",
   },
   {
     "userId": 2,
     "id": 2,
     "userName": "Aadeel",
     "img": "http://placeimg.com/640/480/any",
-    "time": "10:10 PM",
-    "unread": 3,
-    "massage": "how are you"
+    "store": "At cinema",
+  },
+  {
+    "userId": 3,
+    "id": 3,
+    "userName": "Emma",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Work",
+  },
+  {
+    "userId": 4,
+    "id": 4,
+    "userName": "Alai",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 5,
+    "id": 5,
+    "userName": "Abba",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 6,
+    "id": 6,
+    "userName": "Sophia",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 7,
+    "id": 7,
+    "userName": "Adnan",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 8,
+    "id": 8,
+    "userName": "Adnan",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 9,
+    "id": 9,
+    "userName": "Ahmed",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 10,
+    "id": 10,
+    "userName": "Adnan",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
+  },
+  {
+    "userId": 11,
+    "id": 11,
+    "userName": "Adnan",
+    "img": "http://placeimg.com/640/480/any",
+    "store": "Hi, i'm use this app",
   }
 ];
 List<List<Map>> date = [chats, store];
