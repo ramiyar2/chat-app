f270d5efcd3a6002d9f52bd6a9d0cd0eb70b3b18
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-17 14:19:39 +0300

    chat page

---
+++ chat_app_flutter2/lib/pages/chat_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
