b5affdb03d98b1e45c2c20256bec069014de43d1
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-17 14:22:58 +0300

    chat page

--- chat_app_flutter2/lib/pages/chat_page.dart
+++ chat_app_flutter2/lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,8 +8,10 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('data'),
+    return Scaffold(
+      body: Container(
+        child: Text('data'),
+      ),
     );
   }
 }
