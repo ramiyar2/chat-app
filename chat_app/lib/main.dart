b3855fb734f43b58e1bb90b5571263b9e2c498f5
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-18 12:50:18 +0300

    main

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,115 +1,102 @@
+import 'package:chat_app_flutter2/sigin/signin.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+//import 'pages/home.dart';
+import 'screens/home_screen.dart';
+import 'sigin/verify.dart';
+
+void main() => runApp(const MyApp());
+
+var green = const Color.fromRGBO(30, 253, 119, 1);
+var dark_green = const Color.fromRGBO(0, 154, 62, 1);
+var dark_blue = const Color.fromRGBO(24, 42, 43, 1);
+var darker_blue = const Color.fromRGBO(1, 26, 27, 1);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          fontFamily: 'Montserrat',
+          hintColor: dark_green,
+          primarySwatch: customColorGreen,
+          scaffoldBackgroundColor: darker_blue,
+          highlightColor: dark_blue,
+          splashColor: dark_green),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const _SplahPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _SplahPage extends StatelessWidget {
+  const _SplahPage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return const SplashScreen();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SigninPage extends StatelessWidget {
+  const _SigninPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    // return Signin(green, dark_green, dark_blue, darker_blue);
+    return HomePage();
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset('assets/img/myLogo.png'),
+      logoSize: 65,
+      title: Text(
+        "Chats",
+        style: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 42,
+          color: green,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: darker_blue,
+      showLoader: true,
+      loaderColor: green,
+      loadingText: Text(
+        "Loading ...",
+        style: TextStyle(
+          color: green,
+        ),
+      ),
+      navigator: const _SigninPage(),
+      durationInSeconds: 3,
     );
   }
 }
+
+MaterialColor customColorGreen = MaterialColor(
+  Color.fromRGBO(30, 253, 119, 1).value,
+  <int, Color>{
+    50: Color.fromRGBO(30, 253, 119, 0.1),
+    100: Color.fromRGBO(30, 253, 119, 0.2),
+    200: Color.fromRGBO(30, 253, 119, 0.3),
+    300: Color.fromRGBO(30, 253, 119, 0.4),
+    400: Color.fromRGBO(30, 253, 119, 0.5),
+    500: Color.fromRGBO(30, 253, 119, 0.6),
+    600: Color.fromRGBO(30, 253, 119, 0.7),
+    700: Color.fromRGBO(30, 253, 119, 0.8),
+    800: Color.fromRGBO(30, 253, 119, 0.9),
+    900: Color.fromRGBO(30, 253, 119, 1),
+  },
+);
