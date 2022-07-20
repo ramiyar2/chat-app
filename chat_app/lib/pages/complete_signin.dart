757fac9f728020451ae1ac84942c5eaf9a1b866d
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-20 16:55:09 +0300

    .

---
+++ chat_app/lib/pages/complete_signin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CompleteSigin extends StatelessWidget {
+  const CompleteSigin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
