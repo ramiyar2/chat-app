722b9471ceeb6daff5af4d6ab891483ba74d94c7
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-20 15:41:08 +0300

    verify

--- chat_app/lib/pages/verify.dart
+++ chat_app/lib/pages/verify.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import '../screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
