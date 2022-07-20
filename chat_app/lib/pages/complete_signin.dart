38ca8e83b13093cac260b8b7991aa6b696422b26
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-07-21 02:38:14 +0300

    add image and crop

--- chat_app/lib/pages/complete_signin.dart
+++ chat_app/lib/pages/complete_signin.dart
@@ -4,6 +4,7 @@ import 'package:chat_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class CompleteSigin extends StatefulWidget {
   late String name;
@@ -21,12 +22,34 @@ class _CompleteSiginState extends State<CompleteSigin> {
   File? pakedImage;
   FechImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-    if (image == null) {
-      return;
+    if (image != null) {
+      CroppedFile? croppedFile = await ImageCropper().cropImage(
+        sourcePath: image.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: 'Cropper',
+              toolbarColor: Colors.deepOrange,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+        ],
+      );
+      if (croppedFile != null) {
+        setState(() {
+          pakedImage = File(croppedFile.path);
+        });
+      }
     }
-    setState(() {
-      pakedImage = File(image.path);
-    });
   }
 
   @override
