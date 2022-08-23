09abf34f8ac513c56d8eaa3dea3850b57fffc159
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-23 07:43:21 +0300

    routes

--- chat_app/lib/data/routes.dart
+++ chat_app/lib/data/routes.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
-import '../pages/signin.dart';
-import '../pages/verify.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/': (context) => SplahPage(),
